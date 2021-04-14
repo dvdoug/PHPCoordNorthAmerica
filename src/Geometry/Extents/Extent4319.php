b7a644cb233aeb7643df1bd5b69dcd0258781b75
Doug Wright <github@dougweb.org>
2021-04-14 20:15:17 +0100

    Add polygons

---
+++ src/Geometry/Extents/Extent4319.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * PHPCoord.
+ *
+ * @author Doug Wright
+ */
+declare(strict_types=1);
+
+namespace PHPCoord\Geometry\Extents;
+
+/**
+ * North America/USA - Wyoming - Wind River reservation.
+ * @internal
+ */
+class Extent4319
+{
+    public function __invoke(): array
+    {
+        return
+        [
+            [
+                [
+                    [-109.5, 43.85], [-109.5, 43.5625], [-109.5, 43.275], [-109.5, 42.9875], [-109.5, 42.7], [-109.1125, 42.7], [-108.725, 42.7], [-108.3375, 42.7], [-107.95, 42.7], [-107.95, 42.9875], [-107.95, 43.275], [-107.95, 43.5625], [-107.95, 43.85], [-108.3375, 43.85], [-108.725, 43.85], [-109.1125, 43.85], [-109.5, 43.85],
+                ],
+            ],
+        ];
+    }
+}
---
+++ src/Geometry/Extents/Extent4478.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * PHPCoord.
+ *
+ * @author Doug Wright
+ */
+declare(strict_types=1);
+
+namespace PHPCoord\Geometry\Extents;
+
+/**
+ * North America/USA - Oregon - Mitchell.
+ * @internal
+ */
+class Extent4478
+{
+    public function __invoke(): array
+    {
+        return
+        [
+            [
+                [
+                    [-120.54991500709, 44.389375960751], [-120.18661541568, 44.389165187871], [-119.82331582428, 44.388954414991], [-119.82037984083, 44.772897054942], [-120.1861792813, 44.773109322379], [-120.55197872177, 44.773321589816], [-120.54991500709, 44.389375960751],
+                ],
+            ],
+        ];
+    }
+}
