6a261d349f998eed2bf4981b5dcd561682426373
Doug Wright <github@dougweb.org>
2022-09-03 21:06:27 +0100

    Fix stray cache

--- src/CoordinateOperation/GTXGEOID12BAmericaSamoaProvider.php
+++ src/CoordinateOperation/GTXGEOID12BAmericaSamoaProvider.php
@@ -12,6 +12,6 @@ class GTXGEOID12BAmericaSamoaProvider implements GridProvider
 {
     public function provideGrid(): GTXGrid
     {
-        return $this->cache ??= new GTXGrid(__DIR__ . '/../../resources/g2012bs0.bin');
+        return new GTXGrid(__DIR__ . '/../../resources/g2012bs0.bin');
     }
 }
